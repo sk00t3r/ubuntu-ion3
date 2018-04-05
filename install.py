6eae9210e53aab6bd3891ed36ac48df367516f29
sk00t3r <johnsone@roadrunner.com>
2018-04-05 11:14:30 -0400

    Update install.py

--- install.py
+++ install.py
@@ -12,65 +12,65 @@ mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
 mem = mem_bytes/(1024.**3)
 
 def iond():
-        if version == "14.04" and mem >= 4:
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu14.sh", "ubuntu14.sh")
-                os.chmod('ubuntu14.sh', 0755)
-                subprocess.call("./ubuntu14.sh", shell=True)
+        if version == "14.04" and mem >= 4 or version == "16.04" and mem >= 4 or version == "16.10" and mem >= 4or version == "17.04" and mem >= 4 or version == "17.10" and mem >= 4:
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu.sh", "ubuntu.sh")
+                os.chmod('ubuntu.sh', 0755)
+                subprocess.call("./ubuntu.sh", shell=True)
                 
-        elif version == "14.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu14_lowram.sh", "ubuntu14_lowram.sh")
-                os.chmod('ubuntu14_lowram.sh', 0755)
-                subprocess.call("./ubuntu14_lowram.sh", shell=True)
+        elif version == "14.04" or version == "16.04" or version == "16.10" or version == "17.04" or version == "17.10":
+                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu_lowram.sh", "ubuntu_lowram.sh")
+                os.chmod('ubuntu_lowram.sh', 0755)
+                subprocess.call("./ubuntu_lowram.sh", shell=True)
         
-        elif version == "16.04" and mem >= 4:
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16.sh", "ubuntu16.sh")
-                os.chmod('ubuntu16.sh', 0755)
-                subprocess.call("./ubuntu16.sh", shell=True)
+#        elif version == "16.04" and mem >= 4:
+#                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16.sh", "ubuntu16.sh")
+#                os.chmod('ubuntu16.sh', 0755)
+#                subprocess.call("./ubuntu16.sh", shell=True)
                               
-        elif version == "16.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16_lowram.sh", "ubuntu16_lowram.sh")
-                os.chmod('ubuntu16_lowram.sh', 0755)
-                subprocess.call("./ubuntu16_lowram.sh", shell=True)
+#        elif version == "16.04":
+#                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16_lowram.sh", "ubuntu16_lowram.sh")
+#                os.chmod('ubuntu16_lowram.sh', 0755)
+#                subprocess.call("./ubuntu16_lowram.sh", shell=True)
                                 
-        elif version == "16.10" and mem >= 4:
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16.sh", "ubuntu16.sh")
-                os.chmod('ubuntu16.sh', 0755)
-                subprocess.call("./ubuntu16.sh", shell=True)
+#        elif version == "16.10" and mem >= 4:
+#                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16.sh", "ubuntu16.sh")
+#                os.chmod('ubuntu16.sh', 0755)
+#                subprocess.call("./ubuntu16.sh", shell=True)
 
-        elif version == "16.10":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16_lowram.sh", "ubuntu16_lowram.sh")
-                os.chmod('ubuntu16_lowram.sh', 0755)
-                subprocess.call("./ubuntu16_lowram.sh", shell=True)
+#       elif version == "16.10":
+#                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16_lowram.sh", "ubuntu16_lowram.sh")
+#                os.chmod('ubuntu16_lowram.sh', 0755)
+#                subprocess.call("./ubuntu16_lowram.sh", shell=True)
         
-        elif version == "17.04" and mem >= 4:
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu17.sh", "ubuntu17.sh")
-                os.chmod('ubuntu17.sh', 0755)
-                subprocess.call("./ubuntu17.sh", shell=True)
+#        elif version == "17.04" and mem >= 4:
+#                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu17.sh", "ubuntu17.sh")
+#                os.chmod('ubuntu17.sh', 0755)
+#                subprocess.call("./ubuntu17.sh", shell=True)
                                 
-        elif version == "17.04":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu17_lowram.sh", "ubuntu17_lowram.sh")
-                os.chmod('ubuntu17_lowram.sh', 0755)
-                subprocess.call("./ubuntu17_lowram.sh", shell=True)
+#        elif version == "17.04":
+#                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu17_lowram.sh", "ubuntu17_lowram.sh")
+#                os.chmod('ubuntu17_lowram.sh', 0755)
+#                subprocess.call("./ubuntu17_lowram.sh", shell=True)
                 
-        elif version == "17.10" and mem >= 4:
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu17.sh", "ubuntu17.sh")
-                os.chmod('ubuntu17.sh', 0755)
-                subprocess.call("./ubuntu17.sh", shell=True)
+#        elif version == "17.10" and mem >= 4:
+#                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu17.sh", "ubuntu17.sh")
+#                os.chmod('ubuntu17.sh', 0755)
+#                subprocess.call("./ubuntu17.sh", shell=True)
                                 
-        elif version == "17.10":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu17_lowram.sh", "ubuntu17_lowram.sh")
-                os.chmod('ubuntu17_lowram.sh', 0755)
-                subprocess.call("./ubuntu17_lowram.sh", shell=True)
+#        elif version == "17.10":
+#                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu17_lowram.sh", "ubuntu17_lowram.sh")
+#                os.chmod('ubuntu17_lowram.sh', 0755)
+#                subprocess.call("./ubuntu17_lowram.sh", shell=True)
         
-        elif version == "18.1" and mem >= 4:
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16.sh", "ubuntu16.sh")
-                os.chmod('ubuntu16.sh', 0755)
-                subprocess.call("./ubuntu16.sh", shell=True)
+#        elif version == "18.1" and mem >= 4:
+#                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16.sh", "ubuntu16.sh")
+#                os.chmod('ubuntu16.sh', 0755)
+#                subprocess.call("./ubuntu16.sh", shell=True)
                 
-        elif version == "18.1":
-                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16_lowram.sh", "ubuntu16_lowram.sh")
-                os.chmod('ubuntu16_lowram.sh', 0755)
-                subprocess.call("./ubuntu16_lowram.sh", shell=True)
+#        elif version == "18.1":
+#                urllib.urlretrieve ("https://raw.githubusercontent.com/sk00t3r/ubuntu-iond3/master/ubuntu16_lowram.sh", "ubuntu16_lowram.sh")
+#                os.chmod('ubuntu16_lowram.sh', 0755)
+#                subprocess.call("./ubuntu16_lowram.sh", shell=True)
            
         else:
                 print("Sorry version not yet supported.")
